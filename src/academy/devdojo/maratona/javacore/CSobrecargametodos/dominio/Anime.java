fbdc5f69532cd689adcf88233861f1f6b14fd494
jrwarg <jrwarg@gmail.com>
2021-12-01 10:54:57 -0300

    Sobrecarga de Métodos

--- src/academy/devdojo/maratona/javacore/CSobrecargametodos/dominio/Anime.java
+++ src/academy/devdojo/maratona/javacore/CSobrecargametodos/dominio/Anime.java
@@ -1,8 +1,48 @@
 package academy.devdojo.maratona.javacore.CSobrecargametodos.dominio;
 
 public class Anime {
+    private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
+
+    public void init (String nome, String tipo, int episodios){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    // Para evitar inconsistência entre métodos aplicados, por exemplo, em horários diferentes na mesma equipe
+    // criamos um novo método = SOBRECARGA DE MÉTODOS
+
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome,tipo,episodios);
+        this.genero = genero;
+    };
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public String getTipo() {
         return tipo;
